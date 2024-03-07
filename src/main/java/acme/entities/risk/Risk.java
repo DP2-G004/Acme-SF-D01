d2092ec2b076804be39f084d5048270625e05fe4
javi <2210jrg@gmail.com>
2024-03-07 22:44:09 +0100

    risk fixed again

--- src/main/java/acme/entities/risk/Risk.java
+++ src/main/java/acme/entities/risk/Risk.java
@@ -4,17 +4,25 @@ package acme.entities.risk;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Transient;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-public class Risk {
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Risk extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
@@ -36,7 +44,7 @@ public class Risk {
 	private Double				probability;
 
 	@NotBlank
-	@Max(100)
+	@Length(max = 100)
 	private String				description;
 
 	@URL
