f64ba23c943b11ca2eeda67ca438400024831ec1
frajimmor2 <frajimmor2@alum.us.es>
2024-04-14 13:01:50 +0200

    Features Banner

---
+++ src/main/java/acme/features/administrator/banner/AdminBannerCreateService.java
@@ -0,0 +1,80 @@
+
+package acme.features.administrator.banner;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Administrator;
+import acme.client.data.models.Dataset;
+import acme.client.helpers.MomentHelper;
+import acme.client.services.AbstractService;
+import acme.entities.banner.Banner;
+
+@Service
+public class AdminBannerCreateService extends AbstractService<Administrator, Banner> {
+
+	@Autowired
+	AdminBannerRepository adminBannerRepository;
+
+
+	@Override
+	public void authorise() {
+
+		Boolean status = super.getRequest().getPrincipal().hasRole(Administrator.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Banner object = new Banner();
+
+		object.setSlogan("");
+		object.setLink("");
+
+		object.setPictureLink("");
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void bind(final Banner object) {
+		assert object != null;
+
+		super.bind(object, "lastInstantiationMoment", "endOfInstantiation", "pictureLink", "slogan", "link");
+	}
+
+	@Override
+	public void validate(final Banner object) {
+		assert object != null;
+
+		if (!super.getBuffer().getErrors().hasErrors("lastInstantiationMoment"))
+			super.state(MomentHelper.isAfter(object.getLastInstantiationMoment(), object.getEndOfInstantiation()), "lastInstantiationMoment", "administrator.banner.form.error.last-instantiation-moment.invalid");
+
+		if (!super.getBuffer().getErrors().hasErrors("periodoFinal")) {
+
+			//Display period must last for at least one week
+			Date maximumDeadline = MomentHelper.deltaFromMoment(object.getLastInstantiationMoment(), 7, ChronoUnit.DAYS);
+			super.state(MomentHelper.isAfter(object.getEndOfInstantiation(), maximumDeadline), "periodoFinal", "administrator.banner.form.error.period.invalid");
+		}
+
+	}
+
+	@Override
+	public void perform(final Banner object) {
+		assert object != null;
+		this.adminBannerRepository.save(object);
+	}
+
+	@Override
+	public void unbind(final Banner object) {
+		assert object != null;
+
+		Dataset dataset = super.unbind(object, "lastInstantiationMoment", "endOfInstantiation", "pictureLink", "slogan", "link");
+		super.getResponse().addData(dataset);
+	}
+
+}
