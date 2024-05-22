7a10d78584dbe0aeed0d29e23816ac3c6ebd0041
iissi_user <antporara@alum.us.es>
2024-05-22 02:53:14 +0200

    refactor done and working properly on tests

--- src/main/java/acme/entities/project/Project.java
+++ src/main/java/acme/entities/project/Project.java
@@ -3,7 +3,9 @@ package acme.entities.project;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -21,6 +23,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "code"), @Index(columnList = "manager_id")
+})
 public class Project extends AbstractEntity {
 	// Serialisation identifier
 
