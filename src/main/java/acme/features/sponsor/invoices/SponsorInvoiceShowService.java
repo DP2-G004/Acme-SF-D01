31507257ff118d6024368dd3fd5aec717fcb1c37
= <javsanmar5@alum.us.es>
2024-06-20 10:59:12 +0200

    Refactor from D05 to D04

--- src/main/java/acme/features/sponsor/invoices/SponsorInvoiceShowService.java
+++ src/main/java/acme/features/sponsor/invoices/SponsorInvoiceShowService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
 import acme.entities.invoice.Invoice;
+import acme.entities.sponsorship.Sponsorship;
 import acme.roles.Sponsor;
 
 @Service
@@ -19,14 +20,17 @@ public class SponsorInvoiceShowService extends AbstractService<Sponsor, Invoice>
 	@Override
 	public void authorise() {
 
-		int invoiceId;
-		invoiceId = super.getRequest().getData("id", int.class);
-
-		Invoice invoice;
-		invoice = this.repository.findInvoiceById(invoiceId);
-
 		boolean status;
-		status = invoice != null && super.getRequest().getPrincipal().hasRole(Sponsor.class);
+		int id;
+		Invoice invoice;
+		Sponsorship sponsorship;
+		Sponsor sponsor;
+
+		id = super.getRequest().getData("id", int.class);
+		invoice = this.repository.findInvoiceById(id);
+		sponsorship = invoice == null ? null : invoice.getSponsorship();
+		sponsor = sponsorship == null ? null : sponsorship.getSponsor();
+		status = invoice != null && super.getRequest().getPrincipal().hasRole(sponsor);
 
 		super.getResponse().setAuthorised(status);
 	}
@@ -35,9 +39,9 @@ public class SponsorInvoiceShowService extends AbstractService<Sponsor, Invoice>
 	public void load() {
 
 		int invoiceId;
-		invoiceId = super.getRequest().getData("id", int.class);
-
 		Invoice invoice;
+
+		invoiceId = super.getRequest().getData("id", int.class);
 		invoice = this.repository.findInvoiceById(invoiceId);
 
 		super.getBuffer().addData(invoice);
