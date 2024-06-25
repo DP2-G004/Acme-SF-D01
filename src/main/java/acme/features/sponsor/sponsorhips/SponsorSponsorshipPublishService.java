68259aa7d68d4408b80de235ad6e1ebb2632e76d
= <javsanmar5@alum.us.es>
2024-06-25 13:14:41 +0200

    Fix dashboard

--- src/main/java/acme/features/sponsor/sponsorhips/SponsorSponsorshipPublishService.java
+++ src/main/java/acme/features/sponsor/sponsorhips/SponsorSponsorshipPublishService.java
@@ -1,12 +1,19 @@
 
 package acme.features.sponsor.sponsorhips;
 
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.data.models.Dataset;
+import acme.client.helpers.MomentHelper;
 import acme.client.services.AbstractService;
 import acme.client.views.SelectChoices;
+import acme.entities.invoice.Invoice;
 import acme.entities.sponsorship.Sponsorship;
 import acme.entities.sponsorship.SponsorshipType;
 import acme.roles.Sponsor;
@@ -53,6 +60,45 @@ public class SponsorSponsorshipPublishService extends AbstractService<Sponsor, S
 	@Override
 	public void validate(final Sponsorship object) {
 		assert object != null;
+
+		// Code already exists
+		if (!super.getBuffer().getErrors().hasErrors("code")) {
+			Sponsorship sponsorship;
+			sponsorship = this.repository.findSponsorshipByCode(object.getCode());
+			super.state(sponsorship == null, "code", "sponsor.sponsorship.form.error.code-already-exists");
+		}
+
+		if (!super.getBuffer().getErrors().hasErrors("amount")) {
+			// Amount must be positive
+			super.state(object.getAmount().getAmount() >= 0., "amount", "sponsor.sponsorship.form.error.amount-must-be-positive");
+
+			// Currency not supported
+			List<Object> acceptedCurrencies = Arrays.asList(this.repository.findSystemCurrency().getAcceptedCurrencies().split("\\s*,\\s*"));
+			super.state(acceptedCurrencies.contains(object.getAmount().getCurrency()), "amount", "sponsor.sponsorship.form.error.currency-not-supported");
+
+			// Publishing
+			double invoicesTotalAmount = 0.;
+			Collection<Invoice> invoices = this.repository.findInvoicesBySponsorshipId(object.getId());
+			for (Invoice invoice : invoices)
+				if (!invoice.getDraftMode())
+					invoicesTotalAmount += invoice.getTotalAmount().getAmount();
+				else
+					super.state(true, "amount", "sponsor.sponsorship.form.error.all-invoices-must-be-published");
+
+			super.state(object.getAmount().getAmount().equals(invoicesTotalAmount), "amount", "sponsor.sponsorship.form.error.total-invoices-amount-must-be-equal-sponsorship-amount");
+		}
+
+		// startDate must be after moment
+		if (!super.getBuffer().getErrors().hasErrors("startDate"))
+			super.state(MomentHelper.isAfter(object.getStartDate(), object.getMoment()), "startDate", "sponsor.sponsorship.form.error.start-date-must-be-after-moment");
+
+		if (!super.getBuffer().getErrors().hasErrors("endDate")) {
+			// End date must be after start date
+			super.state(MomentHelper.isAfter(object.getEndDate(), object.getStartDate()), "startDate", "sponsor.sponsorship.form.error.endDate-must-be-after-startDate");
+
+			// At least 1 month between start and end
+			super.state(MomentHelper.isLongEnough(object.getStartDate(), object.getEndDate(), 1, ChronoUnit.MONTHS), "startDate", "sponsor.sponsorship.form.error.duration-not-enough");
+		}
 	}
 
 	@Override
