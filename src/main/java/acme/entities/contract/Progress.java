a5057baa3b4ef8a5148cb85cc6a55311de663439
JoseGTD <josgardel8@alum.us.es>
2024-07-05 20:10:07 +0200

    pero hecho hecho

--- src/main/java/acme/entities/contract/Progress.java
+++ src/main/java/acme/entities/contract/Progress.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -24,6 +26,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(indexes = {
+	@Index(columnList = "completeness"), @Index(columnList = "record"), @Index(columnList = "contract_id")
+})
 public class Progress extends AbstractEntity {
 
 	protected static final long	serialVersionUID	= 1L;
--- src/main/webapp/WEB-INF/views/authenticated/contract/list.jsp
+++
@@ -1,9 +0,0 @@
-<%@page language="java"%>
-
-<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@taglib prefix="acme" uri="http://acme-framework.org/"%>
-
-<acme:list>
-	<acme:list-column code="authenticated.contract.list.label.code" path="code"/>
-	<acme:list-column code="authenticated.contract.list.label.draftMode" path="draftMode"/>
-</acme:list>
